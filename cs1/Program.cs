d69eec28429c7cf4762ed454fb7afbfbe2b28372
Lianuch <a.nyzhnyk@nltu.lviv.ua>
2023-03-18 21:52:45 +0200

    cs1

---
+++ cs1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class Circle
+{
+
+    private float square;
+    private float length;
+
+
+    public float Rad { get; set; }
+
+
+
+    public float Len
+    {
+        get { return length; }
+        set { length = (float)(2 * Math.PI * Rad); }
+    }
+
+
+    public float Square
+    {
+        get { return square; }
+        set { square = (float)Math.PI * (Rad * Rad); }
+    }
+
+
+    public bool Check()
+    {
+        if (Rad >= 0 && Rad <= 12)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+    public override string ToString()
+        => "Radius:" + Rad + "\nLength:" + length + "\nSquare:" + square;
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+
+        Circle circle = new Circle();
+        circle.Rad = 11.99f;
+
+        if (circle.Check())
+        {
+            circle.Len = circle.Rad;
+            circle.Square = circle.Rad;
+
+            Console.WriteLine(circle);
+        }
+        else
+        {
+            Console.WriteLine("Incorrect radius value!");
+        }
+
+        Console.ResetColor();
+        Console.ReadKey();
+    }
+}
